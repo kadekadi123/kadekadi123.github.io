3ca7b671b1606963d0d30392fb3efe1d8b279ccc
kadekadi123 <84919935+kadekadi123@users.noreply.github.com>
2021-06-17 23:39:34 +0800

    Add files via upload

---
+++ form.php
@@ -0,0 +1,20 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from='https://kadekadi16.netlify.app/';
+$email_subject='New Form Submission';
+$email_body="User Name: $name.\n".
+            "User Email: $visitor_email.\n".
+            "Subject: $subject.\n".
+            "Message: $message.\n";
+
+$to = 'dekadi16juli@gmail.com';
+$headers = "From: $email_from\r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location: contact.html")
+
+?>
